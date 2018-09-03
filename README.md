30349a2bfef28e024d9b369276fe621a0a42f2f5
Jacob Phillips <jifalops@gmail.com>
2018-09-03 00:46:18 -0400

    Move `example/main.dart` to `example/lib/main.dart`

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 A Flutter widget that wraps a TextFormField and assists with autocomplete functionality.
 
-[example/main.dart](example/main.dart)
+[example/lib/main.dart](example/lib/main.dart)
 
 ![demo.gif](demo.gif)
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,109 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:simple_autocomplete_formfield/simple_autocomplete_formfield.dart';
 
-void main() => runApp(new MyApp());
+final people = <Person>[Person('Alice', '123 Main'), Person('Bob', '456 Main')];
+final letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+class _MyHomePageState extends State<MyHomePage> {
+  String selectedLetter;
+  Person selectedPerson;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final formKey = GlobalKey<FormState>();
+  bool autovalidate = false;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(title: Text(title)),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Form(
+          key: formKey,
+          autovalidate: autovalidate,
+          child: ListView(children: <Widget>[
+            SizedBox(height: 16.0),
+            Text('Selected person: "$selectedPerson"'),
+            Text('Selected letter: "$selectedLetter"'),
+            SizedBox(height: 16.0),
+            SimpleAutocompleteFormField<Person>(
+              decoration: InputDecoration(
+                  labelText: 'Person', border: OutlineInputBorder()),
+              suggestionsHeight: 80.0,
+              itemBuilder: (context, person) => Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(person.name,
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text(person.address)
+                        ]),
+                  ),
+              onSearch: (search) async => people
+                  .where((person) =>
+                      person.name
+                          .toLowerCase()
+                          .contains(search.toLowerCase()) ||
+                      person.address
+                          .toLowerCase()
+                          .contains(search.toLowerCase()))
+                  .toList(),
+              itemFromString: (string) => people.singleWhere(
+                  (person) => person.name.toLowerCase() == string.toLowerCase(),
+                  orElse: () => null),
+              onChanged: (value) => setState(() => selectedPerson = value),
+              onSaved: (value) => setState(() => selectedPerson = value),
+              validator: (person) => person == null ? 'Invalid person.' : null,
+            ),
+            SizedBox(height: 16.0),
+            SimpleAutocompleteFormField<String>(
+              decoration: InputDecoration(
+                  labelText: 'Letter', border: OutlineInputBorder()),
+              // suggestionsHeight: 200.0,
+              maxSuggestions: 10,
+              itemBuilder: (context, item) => Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(item),
+                  ),
+              onSearch: (String search) async => search.isEmpty
+                  ? letters
+                  : letters
+                      .where((letter) => search.toLowerCase().contains(letter))
+                      .toList(),
+              itemFromString: (string) => letters.singleWhere(
+                  (letter) => letter == string.toLowerCase(),
+                  orElse: () => null),
+              onChanged: (value) => setState(() => selectedLetter = value),
+              onSaved: (value) => setState(() => selectedLetter = value),
+              validator: (letter) => letter == null ? 'Invalid letter.' : null,
+            ),
+            SizedBox(height: 16.0),
+            RaisedButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  if (formKey.currentState.validate()) {
+                    formKey.currentState.save();
+                    scaffoldKey.currentState
+                        .showSnackBar(SnackBar(content: Text('Fields valid!')));
+                  } else {
+                    scaffoldKey.currentState.showSnackBar(
+                        SnackBar(content: Text('Fix errors to continue.')));
+                    setState(() => autovalidate = true);
+                  }
+                })
+          ]),
+        ),
+      ));
+}
+
+class Person {
+  Person(this.name, this.address);
+  final String name, address;
+  @override
+  String toString() => name;
+}
+
+void main() => runApp(MyApp());
+const title = 'simple_autocomplete_formfield example';
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      title: title,
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  _MyHomePageState createState() => _MyHomePageState();
 }
