c71d566e9922e7c5f7714fc57a1db028e01e5a6a
Emre Yurtseven <emreyurtseven18@gmail.com>
2022-06-04 11:15:25 +0200

    add data file

---
+++ example/lib/constants/data.dart
@@ -0,0 +1,8 @@
+import 'package:example/models/person.dart';
+
+/// App title
+const title = 'simple_autocomplete_formfield example';
+
+/// Data for the forms
+final people = <Person>[Person('Alice', '123 Main'), Person('Bob', '456 Main')];
+final letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
